87050735319c2ea5bccfc6e63c8305787da6adcd
andrewdibs <Andrew.DiBella1@marist.edu>
2020-03-15 19:37:34 -0400

    created structure and build using react framework

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>mia mia mia mia mia</h1>
+        <h3>u r cool.</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
