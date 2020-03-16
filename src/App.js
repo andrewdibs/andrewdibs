e05921c7224ef4ad88c870500218cdb07a36336a
andrewdibs <Andrew.DiBella1@marist.edu>
2020-03-16 14:56:48 -0400

    added dependencies and start of file structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Home} from './Home';
+import {About} from './About';
+import {Projects} from './Projects';
+import {Page404} from './Page404';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>mia mia mia mia mia</h1>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <React.Fragment>
+        <Router>
+          <Switch>
+            <Route exact path= "/" component={Home} />
+            <Route path= "/" component={About} />
+            <Route path= "/" component={Projects} />
+            <Route component={Page404} />
+          </Switch>
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
+
 
 export default App;
